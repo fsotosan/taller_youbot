12dca80155d748eab860788ebc81205ed8658530
fsotosan <youbot@linux.local>
2013-04-06 17:39:33 +0200

    vel_publisher 	new file:   CMakeLists.txt 	new file:   Makefile 	new file:   bin/vel_publisher 	new file:   build/Makefile 	new file:   build/cmake_install.cmake 	new file:   mainpage.dox 	new file:   manifest.xml 	new file:   src/vel_publisher.cpp

---
+++ build/CMakeFiles/vel_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/vel_publisher.dir/src/vel_publisher.o"
+  "../bin/vel_publisher.pdb"
+  "../bin/vel_publisher"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/vel_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
