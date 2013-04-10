7e790510884fe05286783f56749e9b782fbf0b7f
youbot <youbot@ubuntu.(none)>
2013-04-11 01:26:12 +0200

    añadida detención de seguridad en youbot_base_move

--- src/youbot_base_move.cpp
+++ src/youbot_base_move.cpp
@@ -15,14 +15,21 @@ using namespace youbot;
 using namespace std;
 
 void velCallback(const geometry_msgs::Twist::ConstPtr& inTwist);
+void timerCallback(const ros::TimerEvent&);
+void moveBase(float inLongV, float inTransV, float inAngV);
+void stopBase();
+
 int i = 0;
 YouBotBase* myYouBotBase = 0;
 bool myYouBotHasBase = false;
+ros::Timer myTimer;
+ros::NodeHandle* myNodeHandle = 0;
 
 int main(int argc, char **argv) {
 
 	ros::init(argc, argv, "youbot_base_move");
 	ros::NodeHandle theNodeHandle;
+	myNodeHandle = &theNodeHandle;
 	ros::Subscriber theSubscriber = theNodeHandle.subscribe("cmd_ref_vel", 10, velCallback);
 
 	ROS_INFO("Programa youbot_base_move iniciado");
@@ -47,14 +54,43 @@ int main(int argc, char **argv) {
 
 void velCallback(const geometry_msgs::Twist::ConstPtr& inTwist) {
 
+	// Paramos el temporizador para evitar la detención automática de la base
+
+	myTimer.stop();
+
+	// Indicamos que se ha recibido un mensaje de velocidad
+
 	ROS_INFO("%d - Recibido mensaje Twist: vLineal = (%g,%g,%g), vAngular = (%g,%g,%g)\n", i++, inTwist->linear.x, inTwist->linear.y, inTwist->linear.z, inTwist->angular.x, inTwist->angular.y, inTwist->angular.z);
+
+	// Ejecutamos la orden
+
+	moveBase(inTwist->linear.x, inTwist->linear.y, inTwist->angular.z);
+
+	// Iniciamos un temporizador para detener el robot si no se reciben nuevas órdenes en un segundo
+
+	myTimer = myNodeHandle->createTimer(ros::Duration(1.0),timerCallback, true);
+
+}
+
+void timerCallback(const ros::TimerEvent&) {
+
+	ROS_INFO("Deteniendo la base por timeout");
+	stopBase();
+}
+
+void stopBase() {
+
+	moveBase(0.0,0.0,0.0);
+}
+
+void moveBase(float inLongV, float inTransV, float inAngV) {
 
 	/*
 	* la api OODL utiliza unidades boost
 	*/
-	quantity<si::velocity> longitudinalVelocity = inTwist->linear.x * meter_per_second;
-	quantity<si::velocity> transversalVelocity = inTwist->linear.y * meter_per_second;
-	quantity<si::angular_velocity> angularVelocity = inTwist->angular.z * radian_per_second;
+	quantity<si::velocity> longitudinalVelocity = inLongV * meter_per_second;
+	quantity<si::velocity> transversalVelocity = inTransV * meter_per_second;
+	quantity<si::angular_velocity> angularVelocity = inAngV * radian_per_second;
 
 	if (myYouBotHasBase) {
 		myYouBotBase->setBaseVelocity(longitudinalVelocity, transversalVelocity, angularVelocity);
